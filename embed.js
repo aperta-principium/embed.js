13c80995444c6191fb3e7f5c99ac60e2cfa2dc59
Filip Troníček <filip.tronicek@seznam.cz>
2019-10-18 15:31:34 +0200

    Update embed.js

--- embed.js
+++ embed.js
@@ -139,7 +139,7 @@ function Embed(url) {
     $("#embed").load(url);
   }
   if(csvCheck(url)) {
-    $.get('/file-path/demo.csv', function(data) {
+    $.get(url, function(data) {
       var build = '<table border="1" cellpadding="2" cellspacing="0" style="border-collapse: collapse" width="100%">\n';
       var head = data.split("\n");
       for(var i=0;i<1;i++){
