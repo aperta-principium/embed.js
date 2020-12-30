61555e0712bdccd70e52767ee729d137bc7f0656
Filip Troníček <filip.tronicek@seznam.cz>
2020-12-30 17:09:41 +0100

    Change the file structure a bit

--- embed.js
+++ embed.js
@@ -1,76 +1,116 @@
-function embed(url, el = "#embed") {
-  function GetVimeoIDbyUrl(url) {
-    let id = false;
-    $.ajax({
-      url: `https://vimeo.com/api/oembed.json?url=${url}`,
-      async: false,
-      success: (response) => {
-        if (response.video_id) {
-          id = response.video_id;
-        }
-      },
-    });
-    return id;
-  } //
+function videoCheck(url) {
+  return url.match(/\.(mp4|mkv)$/i) != null;
+}
 
-  function valUrl() {
-    if (url != undefined || url != "") {
-      const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=)([^#\&\?]*).*/i;
-      const match = url.match(regExp);
-      if (match && match[2].length == 11) {
-        // Do anything for being valid
-        // if need to change the url to embed url then use below line)
+function documentCheck(url) {
+  return (
+    url.match(/\.(doc|docx|xls|xlsx|ppt|pptx|pdf|pages|eps|ps|ttf|xps)$/i) !=
+    null
+  );
+}
+
+function textCheck(url) {
+  return url.match(/\.(txt)$/i) != null;
+}
+
+function musicCheck(url) {
+  return url.match(/\.(mp3|waw|ogg)$/i) != null;
+}
+
+
+/**
+ * @param  {} url
+ * @param  {} callback
+ * @param  {} timeout
+ */
+function testImage(url, callback, timeout) {
+  timeout = timeout || 5000;
+  let timedOut = false,
+    timer;
+  const img = new Image();
+  img.onerror = img.onabort = () => {
+    if (!timedOut) {
+      clearTimeout(timer);
+      callback(url, "error");
+    }
+  };
+  img.onload = () => {
+    if (!timedOut) {
+      clearTimeout(timer);
+      callback(url, "success");
+    }
+  };
+  timer = setTimeout(() => {
+      timedOut = true;
+      callback(url, "timeout");
+    }, timeout);
+}
+
+
+/* Vimeo and YouTube */
+
+function vimYTvalidate() {
+  if (url != undefined || url != "") {
+    const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=|\?v=)([^#\&\?]*).*/i;
+    const match = url.match(regExp);
+    if (match && match[2].length == 11) {
+      // Do anything for being valid
+      // if need to change the url to embed url then use below line)
+      $(el).html(
+        '<iframe id="yt" width="100%" height="500" frameborder="0"> </iframe>'
+      );
+
+      $("#yt").attr(
+        "src",
+        `https://www.youtube.com/embed/${match[2]}?autoplay=0&rel=0`
+      );
+    } else {
+      if (GetVimeoIDbyUrl(url)) {
+        id = GetVimeoIDbyUrl(url);
         $(el).html(
-          '<iframe id="yt" width="100%" height="500" frameborder="0"> </iframe>'
+          '<iframe id="vimeoPlayer" src="" width="100%" height="500" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>'
         );
-
-        $("#yt").attr(
+        $("#vimeoPlayer").attr(
           "src",
-          `https://www.youtube.com/embed/${match[2]}?autoplay=0&rel=0`
+          `//player.vimeo.com/video/${id}?title=0&amp;byline=0&amp;portrait=0&amp;color=ffff00"`
         );
       } else {
-        if (GetVimeoIDbyUrl(url)) {
-          id = GetVimeoIDbyUrl(url);
-          $(el).html(
-            '<iframe id="vimeoPlayer" src="" width="100%" height="500" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>'
-          );
-          $("#vimeoPlayer").attr(
-            "src",
-            `//player.vimeo.com/video/${id}?title=0&amp;byline=0&amp;portrait=0&amp;color=ffff00"`
-          );
-        } else {
-          $("#yt").hide();
-        }
-        // Do anything for not being valid
+        $("#yt").hide();
       }
-    } else {
-      $("#player").hide();
-      $("#imgShow").hide();
       // Do anything for not being valid
     }
+  } else {
+    $("#player").hide();
+    $("#imgShow").hide();
+    // Do anything for not being valid
   }
-  function testImage(url, callback, timeout) {
-    timeout = timeout || 5000;
-    let timedOut = false,
-      timer;
-    const img = new Image();
-    img.onerror = img.onabort = () => {
-      if (!timedOut) {
-        clearTimeout(timer);
-        callback(url, "error");
+}
+/**
+ * Used to get video id from Vimeo URL
+ * @param  {} url
+ */
+
+function GetVimeoIDbyUrl(url) {
+  let id = false;
+  $.ajax({
+    url: `https://vimeo.com/api/oembed.json?url=${url}`,
+    async: false,
+    success: (response) => {
+      if (response.video_id) {
+        id = response.video_id;
       }
-    };
-    img.onload = () => {
-      if (!timedOut) {
-        clearTimeout(timer);
-        callback(url, "success");
-      }
-    };
-    timer = setTimeout(() => {
-        timedOut = true;
-        callback(url, "timeout");
-      }, timeout);
-  }
+    },
+  });
+  return id;
+}
+
+
+/**
+ * @param  {} url
+ * @param  {} el="#embed" - HTML selector
+ */
+function embed(url, el = "#embed") {
+
   function record(url, result) {
     if (result == "success") {
       $("#imgShow").attr("src", `https://interclip.app/proxy?url=${url}`);
@@ -78,22 +118,7 @@ function embed(url, el = "#embed") {
       $("#imgShow").hide();
     }
   }
-  function videoCheck(url) {
-    return url.match(/\.(mp4|mkv)$/i) != null;
-  }
 
-  function documentCheck(url) {
-    return (
-      url.match(/\.(doc|docx|xls|xlsx|ppt|pptx|pdf|pages|eps|ps|ttf|xps)$/i) !=
-      null
-    );
-  }
-  function textCheck(url) {
-    return url.match(/\.(txt)$/i) != null;
-  }
-  function musicCheck(url) {
-    return url.match(/\.(mp3|waw|ogg)$/i) != null;
-  }
   if (videoCheck(url)) {
     $(el).html(
       `<video id="player" width="100%" playsinline controls><source src="${url}" id="videoSource"/></video>`
@@ -101,6 +126,7 @@ function embed(url, el = "#embed") {
   } else {
     $("#player").hide();
   }
+
   if (documentCheck(url)) {
     $(el).html(
       "<iframe id='documentEmbed' src='' width='100%' height='623px' frameborder='0'>"
@@ -112,12 +138,14 @@ function embed(url, el = "#embed") {
   } else {
     $("#documentEmbed").hide();
   }
+
   if (musicCheck(url)) {
     $(el).html(`<audio controls><source src="${url}"></audio> `);
   }
   if (textCheck(url)) {
     $(el).load(url);
   }
+
   testImage(url, record);
-  valUrl();
+  vimYTvalidate();
 }
