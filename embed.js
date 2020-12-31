deeb0add007844eae08c1548180a41148f6ed7ae
Filip Troníček <filip.tronicek@seznam.cz>
2020-12-31 22:58:40 +0100

    Custom hide function

--- embed.js
+++ embed.js
@@ -1,4 +1,12 @@
-import {videoCheck, documentCheck, musicCheck, textCheck, testImage, vimYTvalidate} from './checks.js';
+import {
+    videoCheck,
+    documentCheck,
+    musicCheck,
+    textCheck,
+    testImage,
+    vimYTvalidate
+} from './checks.js';
+import { hide } from './pollyfills.js';
 
 /**
  * @param  {} url
@@ -6,39 +14,34 @@ import {videoCheck, documentCheck, musicCheck, textCheck, testImage, vimYTvalida
  */
 export function embed(url, el = "#embed") {
 
-  function record(url, result) {
-    if (result == "success") {
-      document.querySelector("#imgShow").attr("src", `https://interclip.app/proxy?url=${url}`);
-    } else {
-      $("#imgShow").hide();
+    function record(url, result) {
+        if (result == "success") {
+            document.querySelector("#imgShow").attr("src", `https://interclip.app/proxy?url=${url}`);
+        } else {
+            hide("#imgShow");
+        }
     }
-  }
 
-  if (videoCheck(url)) {
-    document.querySelector(el).innerHTML = 
-      `<video id="player" width="100%" playsinline controls><source src="${url}" id="videoSource"/></video>`;
-  } else {
-    document.querySelector("#player").hide();
-  }
+    if (videoCheck(url)) {
+        document.querySelector(el).innerHTML = `<video id="player" width="100%" playsinline controls><source src="${url}" id="videoSource"/></video>`;
+    } else {
+        hide("#player");
+    }
 
-  if (documentCheck(url)) {
-    document.querySelector(el).innerHTML = 
-      "<iframe id='documentEmbed' width='100%' height='623px' frameborder='0'>";
-    document.querySelector("#documentEmbed").attr(
-      "src",
-      `https://drive.google.com/viewerng/viewer?embedded=true&url=${url}`
-    );
-  } else {
-    document.querySelector("#documentEmbed").hide();
-  }
+    if (documentCheck(url)) {
+        document.querySelector(el).innerHTML = "<iframe id='documentEmbed' width='100%' height='623px' frameborder='0'>";
+        document.querySelector("#documentEmbed").attr("src", `https://drive.google.com/viewerng/viewer?embedded=true&url=${url}`);
+    } else {
+        hide("#documentEmbed");
+    }
 
-  if (musicCheck(url)) {
-    document.querySelector(el).innerHTML = `<audio controls><source src="${url}"></audio> `;
-  }
-  if (textCheck(url)) {
-    document.querySelector(el).load(url);
-  }
+    if (musicCheck(url)) {
+        document.querySelector(el).innerHTML = `<audio controls><source src="${url}"></audio> `;
+    }
+    if (textCheck(url)) {
+        document.querySelector(el).load(url);
+    }
 
-  testImage(url, record);
-  vimYTvalidate(url);
+    testImage(url, record);
+    vimYTvalidate(url);
 }
---
+++ pollyfills.js
@@ -0,0 +1,7 @@
+function hide(el) {
+    document.querySelector(el).style.display = "none";
+}
+
+function hide(el) {
+    document.querySelector(el).style.display = "block";
+}
